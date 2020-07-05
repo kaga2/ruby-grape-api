b943c43611bde52cd9dc14527d865f06b11b4976
Jose Carlos Gonzalez Fernandez <jcfernandez@kueski.com>
2020-07-05 14:24:57 -0500

    Rename routes with resources at api_versions folders

--- api_versions/v1/resources/example.rb
+++ api_versions/v1/resources/example.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require './business/example'
+
 module APIVersions
   module V1
     module Routes
@@ -10,6 +12,7 @@ module APIVersions
             requires :example_param, type: String, allow_blank: false, desc: 'Example param'
           end
           get do
+            Business::Example.instance.do_something
             { result: "Example response: example_param = #{params[:example_param]}" }
           end
         end
