86fe6378a8a554857098b0c246a7481e75ce56c2
Jose Carlos Gonzalez Fernandez <jcfernandez@kueski.com>
2020-07-05 13:47:56 -0500

    Add the Example API v1 with all the routes included

---
+++ api_versions/v1/api.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require_relative 'routes/example'
+require_relative 'response_decorators/jsend'
+
+module APIVersions
+  module V1
+    # Example API v1
+    class API < Grape::API
+      version :v1
+      include ResponseDecorators::JSend
+
+      mount Routes::Example
+    end
+  end
+end
