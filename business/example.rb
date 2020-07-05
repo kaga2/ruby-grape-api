a435fc187b195405498ef675ed5c716ce7ac939f
Jose Carlos Gonzalez Fernandez <jcfernandez@kueski.com>
2020-07-05 14:25:53 -0500

    Add example business class with example method

---
+++ business/example.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Business
+  # Example class with the business logic
+  class Example
+    include Singleton
+
+    def do_something
+      'not today'
+    end
+  end
+end
